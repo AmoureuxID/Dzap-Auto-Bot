690b741cfdfa62125de7700863fd108691635c87
AmoureuxID <134390773+AmoureuxID@users.noreply.github.com>
2025-07-14 12:34:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
 export const config = {
-
-    sepoliaRpcUrl: "https://eth-sepolia.g.alchemy.com/v2/Onk45xXyTWVFFNDbJvP5R",
-    arbitrumSepoliaRpcUrl: "https://arb-sepolia.g.alchemy.com/v2/Onk45xXyTWVFFNDbJvP5R",
-	
-    ethToArbCount: 10,
-    arbToEthCount: 5,
-	
-    ethToArbAmount: "0.001",
-    arbToEthAmount: "0.0001",
-
+    sepoliaRpcUrl: "https://eth-sepolia.g.alchemy.com/v2/YOUR_SEPOLIA_API_KEY",
+    arbitrumSepoliaRpcUrl: "https://arb-sepolia.g.alchemy.com/v2/YOUR_ARBITRUM_API_KEY",
+    ethToArbCount: 3,
+    arbToEthCount: 2,
+    bridgeAmount: "0.0001",
     delayBetweenTxSeconds: 30,
-
-    loopIntervalMinutes: 1,
+    loopIntervalMinutes: 120,
 };
